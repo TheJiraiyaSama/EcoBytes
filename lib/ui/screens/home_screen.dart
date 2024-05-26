ca3f72137b86b38a4af4f951f575a9b656891615
Surabhi Raghavan <surabhiraghavan1@gmail.com>
2024-05-26 17:37:23 +0530

    simple code change

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -6,7 +6,7 @@ import 'package:ecobytes/ui/screens/services.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
