f8cb7b5e9770bf54a2380cf4dfda48c6d1ff0248
TheJiraiyaSama <rohitkane001@gmail.com>
2024-05-26 18:51:40 +0530

    Camera

--- lib/ui/screens/services.dart
+++ lib/ui/screens/services.dart
@@ -1,15 +1,80 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class ServicesPage extends StatelessWidget {
+class ServicesPage extends StatefulWidget {
   const ServicesPage({super.key});
+
+  @override
+  _ServicesPageState createState() => _ServicesPageState();
+}
+
+class _ServicesPageState extends State<ServicesPage> {
+  CameraController? _controller;
+  List<CameraDescription>? cameras;
+  bool _isCameraInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    cameras = await availableCameras();
+    if (cameras != null && cameras!.isNotEmpty) {
+      _controller = CameraController(
+        cameras![0],
+        ResolutionPreset.high,
+      );
+
+      _controller?.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {
+          _isCameraInitialized = true;
+        });
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff99A78E),
-      body: Center(
-        child: Text("PAGE 4"),
-      ),
+      body: _isCameraInitialized
+          ? Stack(
+              children: [
+                CameraPreview(_controller!),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: FloatingActionButton(
+                      onPressed: () async {
+                        if (_controller != null && _controller!.value.isInitialized) {
+                          try {
+                            final image = await _controller!.takePicture();
+                            // Handle the captured image, e.g., display it or save it
+                            print('Picture saved at ${image.path}');
+                          } catch (e) {
+                            print('Error capturing image: $e');
+                          }
+                        }
+                      },
+                      child: Icon(Icons.camera),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : Center(child: CircularProgressIndicator()),
     );
   }
 }
